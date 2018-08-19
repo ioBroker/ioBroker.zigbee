958c34257dd0c99f1fb8065a67ac1255382dc2e6
arteck <6681528+arteck@users.noreply.github.com>
2018-08-19 21:18:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -179,6 +179,7 @@ function updateState(devId, name, value, common) {
                 }
             }
             // check if state exist
+                        
             adapter.getObject(id, function(err, stobj) {
                 if (stobj) {
                     // update state - not change name and role (user can it changed)
@@ -187,11 +188,22 @@ function updateState(devId, name, value, common) {
                 }
                 adapter.extendObject(id, {type: 'state', common: new_common});
                 adapter.setState(id, value, true);
+                
+                wait(500);
+                
             });
         } else {
             adapter.log.debug('Wrong device '+devId);
         }
     });
+}
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
 }
 
 
