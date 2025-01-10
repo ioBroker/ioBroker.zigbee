eaae381188be7f3b617c39e5c2f4240c21a656e4
asgothian <45667167+asgothian@users.noreply.github.com>
2025-01-10 22:17:06 +0100

    Update main.js
    change log message

--- main.js
+++ main.js
@@ -692,14 +692,18 @@ class Zigbee extends utils.Adapter {
                                 if (converter.hasOwnProperty('convertGet')) {
                                     for (const ckey of converter.key) {
                                         try {
+                                            const t = Date.getTime();
                                             await converter.convertGet(entity.device.endpoints[0], ckey, {});
                                         } catch (error) {
-                                            this.log.warn(`Failed to read state (1)'${JSON.stringify(ckey)}'of '${entity.device.ieeeAddr}/${entity.device.endpoints[0].ID} ' after query with '${error && error.message ? error.message : 'no error message'}`);
+                                            const delta = Date.getTime()-t;
+                                            this.log.warn(`Failed to read state (1)'${JSON.stringify(ckey)}'of '${entity.device.ieeeAddr}/${entity.device.endpoints[0].ID} ' after ${delta} ms from query with '${error && error.message ? error.message : 'no error message'}`);
                                         }
                                         try {
+                                            const t = Date.getTime();
                                             await converter.convertGet(entity.device, ckey, {});
                                         } catch (error) {
-                                            this.log.warn(`Failed to read state (2)'${JSON.stringify(ckey)}'of '${entity.device.ieeeAddr}' after query with '${error && error.message ? error.message : 'no error message'}`);
+                                            const delta = Date.getTime()-t;
+                                            this.log.warn(`Failed to read state (2)'${JSON.stringify(ckey)}'of '${entity.device.ieeeAddr}' after ${delta} ms from query with '${error && error.message ? error.message : 'no error message'}`);
                                         }
                                     }
                                 }
