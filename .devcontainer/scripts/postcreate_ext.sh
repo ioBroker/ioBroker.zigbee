c571846aa8ccec572909ebc7b2ba51ab171c87d4
asgothian <45667167+asgothian@users.noreply.github.com>
2025-08-27 22:34:34 +0200

    fix devcontainer

--- .devcontainer/scripts/postcreate_ext.sh
+++ .devcontainer/scripts/postcreate_ext.sh
@@ -10,5 +10,4 @@ iob object set system.adapter.zigbee.0 native.panID=3788
 iob object set system.adapter.zigbee.0 native.extPanID=c8bb26fe53dc70e3
 iob object set system.adapter.zigbee.0 native.precfgkey=01030507090B0D0F00020406080A0C0D
 iob object set system.adapter.zigbee.0 native.debugHerdsman=true
-iob object set system.adapter.zigbee.0 native.autostart=true
-
+iob object set system.adapter.zigbee.0 native.autostart=false
